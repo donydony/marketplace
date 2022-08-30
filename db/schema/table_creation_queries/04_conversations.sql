ed68241e56c68082f8a048864e9b18d9fcdf9565
Daniel Suen <danielsuen200791357@gmail.com>
2022-08-30 06:59:58 +0000

    added unique contraint to conversations table

--- db/schema/table_creation_queries/04_conversations.sql
+++ db/schema/table_creation_queries/04_conversations.sql
@@ -3,5 +3,6 @@ CREATE TABLE conversations (
   id SERIAL PRIMARY KEY NOT NULL,
   sender_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
   receiver_id INTEGER REFERENCES users(id) ON DELETE CASCADE NOT NULL,
-  item_id INTEGER REFERENCES items(id) ON DELETE CASCADE NOT NULL
+  item_id INTEGER REFERENCES items(id) ON DELETE CASCADE NOT NULL,
+  UNIQUE (sender_id, receiver_id, item_id)
 );
