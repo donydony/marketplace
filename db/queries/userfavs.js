c8771eb90b60ffe6a3e7e5af97cefc4d75c67879
Daniel Suen <danielsuen200791357@gmail.com>
2022-09-02 01:51:43 +0000

    added stuff

--- db/queries/userfavs.js
+++ db/queries/userfavs.js
@@ -4,10 +4,9 @@ const db = require('../connection');
 
 let favData = function (userName) {
   return db
-    .query(`SELECT items.id as item_id, img_url, items.sold_status, price, title, items.description AS item_description, favourites.id AS favourites_id
+    .query(`SELECT items.id AS item_id, img_url, items.sold_status, price, title, items.description AS item_description
     FROM items
     JOIN users ON items.seller_id = users.id
-    LEFT JOIN favourites ON favourites.user_id = users.id
     WHERE users.username = $1
     LIMIT 10`, [userName])
     .then(data => {
